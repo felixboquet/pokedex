acd20ee56fb19430d644a3a74c06e74e42cde277
Felix Boquet <boquet@brocelia.fr>
2020-06-27 16:06:27 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { Pokemon } from "./shared/pokemon/pokemon.model";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "./list/list.component.html",
+  styleUrls: ["./list/list.component.css"]
+})
+export class AppComponent { }
